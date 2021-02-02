ba9608b18eb09395cee864b63125a0964a92002e
Abishek G <abishekg2712@gmail.com>
2021-02-02 12:36:54 +0530

    context

--- app/routes/sales-desk/index.ts
+++ app/routes/sales-desk/index.ts
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
+import {customExpressRequest} from '../../@types/express';
+
 const router = express.Router();
 import path from 'path';
 
-export default router.get('/', (req, res) => {
+export default router.get('/', (req: customExpressRequest, res: Response) => {
     res.sendFile(path.join(__dirname + "../../../../public/index.html"));
 })
